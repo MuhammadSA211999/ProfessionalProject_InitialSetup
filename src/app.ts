29f79d8fe62d1d7159999342df9343167907bd84
Muhammad SA <MuhammadSA211999>
2023-07-30 09:58:49 +0600

    [DONE] initial instalation and setup

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+import express, { urlencoded, Application, Request, Response } from 'express'
+import cors from 'cors'
+const app: Application = express()
+app.use(cors())
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
+
+app.get('/', async (req: Request, res: Response) => {
+    res.json('PROJECT STARTED ON BROWSWER').send("University-management project on browser")
+})
+
+export default app
---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import app from './app'
+import config from "./confiq/index";
+
+async function main() {
+    try {
+        await mongoose.connect(config.database_url as string)
+        console.log('Database connect successfully')
+        app.listen(config.port, () => {
+            console.log(`Application listen on port ${config.port}`)
+        })
+    } catch (err) {
+        console.log('Database does not connect and error was', err)
+    }
+}
+
+main()
